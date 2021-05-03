368b39bd85b48d4a457757361c58106186d64f5b
saaitt <saeid.firouzi@gmail.com>
2021-05-03 21:07:20 +0430

    fix migrations typo

--- migrations/V1.1__Create_Marketing_Product_Table.sql
+++ migrations/V1.1__Create_Marketing_Product_Table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE marketing_products (
     id SERIAL PRIMARY KEY,
     countUsage INT,
-    UUID varchar(200) NOT NULL,
+    uuid varchar(200) NOT NULL,
     productId INT ,
     FOREIGN KEY (productId) REFERENCES products(id)
 );
